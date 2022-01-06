2db3ab30aff48a1a72345866e4a7aad7ec5d66b3
jameskelly33 <jameskelly33@gmail.com>
2022-01-06 10:10:09 +0000

    Setup allauth

--- language_school/settings.py
+++ language_school/settings.py
@@ -85,6 +85,15 @@ WSGI_APPLICATION = 'language_school.wsgi.application'
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
