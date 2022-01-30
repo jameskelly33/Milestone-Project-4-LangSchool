5eb73537d1500c38f6a257e1bc57d0a160156501
jameskelly33 <jameskelly33@gmail.com>
2022-01-30 14:38:56 +0000

    Add basic functionality to show level test

--- level_test/views.py
+++ level_test/views.py
@@ -1,6 +1,60 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def level_test(request):
+   
+    question_counter = 1
+    question = Question.objects.get(question_number=question_counter)
+    total = 0
     
-    return render(request, 'level_test/level_test.html')
+    context = {
+        'question': question,
+        'question_counter':question_counter,
+        'total':total
+    }
+    return render(request, 'level_test/level_test.html', context)
+
+    
+def check_answer(request, question_counter, total):
+    
+    total_qs = Question.objects.all().count()
+    current_question = Question.objects.get(question_number=question_counter)
+    correct_answer = current_question.correct_answer
+    if question_counter == total_qs:
+        
+        question= Question.objects.get(question_number=question_counter)
+        answer = request.GET['answer']
+        if answer == correct_answer:
+            total +=10
+            context={
+                'total':total,
+            }
+            return render(request, 'level_test/level_test_results.html', context)
+        else:
+
+            context={
+                'total':total,
+            }
+            return render(request, 'level_test/level_test_results.html', context)
+       
+    else:
+        question_counter += 1
+        question= Question.objects.get(question_number=question_counter)
+        answer = request.GET['answer']
+        if answer == correct_answer:
+            total +=10
+        
+            
+        
+        context = {
+                'question':question,
+                'current_question':current_question,
+                'correct_answer':correct_answer,
+                'answer': answer,
+                'question_counter':question_counter,
+                'total':total,
+                'total_qs':total_qs
+            }
+        return render(request, 'level_test/level_test.html', context)
+
