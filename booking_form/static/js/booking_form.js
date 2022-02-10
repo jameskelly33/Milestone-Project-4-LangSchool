6f0b75b8ea47e005ec1801eeb776b0c42d05901e
jameskelly33 <jameskelly33@gmail.com>
2022-02-10 21:23:14 +0000

    Add calculate total function for course details view

---
+++ booking_form/static/js/booking_form.js
@@ -0,0 +1,19 @@
+// Update Total Course Cost 
+const courseLength = document.getElementById('id_course_length')
+const totalCourseCost = document.getElementById('total_course_cost')
+const courseCostPerWeek =totalCourseCost.textContent
+
+let currentTotal  
+
+courseLength.addEventListener("click", calculateTotal);
+
+function calculateTotal(){
+
+    currentTotal = parseFloat(courseLength.value) * parseFloat(courseCostPerWeek)
+    totalCourseCost.innerText = currentTotal.toFixed(2)
+}
+
+
+
+
+
