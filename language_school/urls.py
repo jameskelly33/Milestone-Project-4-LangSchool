1f367d332772ab81c72802f66661a9c9bd714bf6
jameskelly33 <jameskelly33@gmail.com>
2022-02-15 13:43:54 +0000

    Add profiles app with urls and basic views.

--- language_school/urls.py
+++ language_school/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('level_test/', include('level_test.urls')),
     path('blog/', include('blog.urls')),
     path('booking_form/', include('booking_form.urls')),
+    path('profiles/', include('profiles.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
