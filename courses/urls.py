598c25e2d1bad1dbd59c9d2fce94d8805a0f9862
jameskelly33 <jameskelly33@gmail.com>
2022-02-16 22:25:58 +0000

    Add URLS for course add, edit and delete.

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.courses, name='courses'),
-    path('course_library/', views.course_library, name='course_library')
+    path('course_library/', views.course_library, name='course_library'),
+    path('add/', views.add_course, name = 'add_course'),
+    path('edit/<course_id>', views.edit_course, name='edit_course'),
+    path('delete/<course_id>', views.delete_course, name='delete_course'),
+
 ]
