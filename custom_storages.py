906d07a53893720e27f2a018f3131e288e2e5e15
jameskelly33 <jameskelly33@gmail.com>
2022-02-21 13:54:15 +0000

    Fix typo in custom_storages.py import statement

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,11 @@
+
 from django.conf import settings
-from storages.backends.sboto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorages(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-
-class MediaStorages(S3Boto3Storage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
