ac5d7131b480be8e0b9a800d9e2eb1c6065db7c7
jameskelly33 <jameskelly33@gmail.com>
2022-02-21 14:29:19 +0000

    Remove duplicate AWS_ACCESS_KEY vairable in settings and replace with correct AWS_SECRET_ACCESS_KEY

--- language_school/settings.py
+++ language_school/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'lang-school-ms4'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
