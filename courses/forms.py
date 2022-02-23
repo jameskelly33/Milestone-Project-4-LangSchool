e7ebf4dcfa0fde751a0c4baade32b475ced74cb4
jameskelly33 <jameskelly33@gmail.com>
2022-02-23 13:57:10 +0000

    Add placholders to add course fields

--- courses/forms.py
+++ courses/forms.py
@@ -11,8 +11,15 @@ class CourseForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Get category options 
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['course_category'].choices = friendly_names
+        # Add custom placeholders to aid user form completion
+        self.fields['name'].widget.attrs['placeholder'] = 'eg. general_english_full_time'
+        self.fields['friendly_name'].widget.attrs['placeholder'] = 'eg. General English Full Time'
+        self.fields['course_timetable'].widget.attrs['placeholder'] = 'eg. Mon-Fri, 9.00 - 14.00'
+
+      
+            
         
