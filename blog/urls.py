843a5b48f1a2660e14c72bb536636e0eef2ef681
jameskelly33 <jameskelly33@gmail.com>
2022-02-25 09:33:49 +0000

    Change blog_post url argument to an integer

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('<post_id>', views.blog_post, name='blog_post'),
+    path('<int:post_id>', views.blog_post, name='blog_post'),
     path('like/<int:post_id>/', views.like, name='like_post'),
     path('add_post/', views.add_post, name='add_post'),
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
