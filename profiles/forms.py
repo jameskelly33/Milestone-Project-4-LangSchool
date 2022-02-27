c95590abb9d41461665d6955d76df1ced1402264
jameskelly33 <jameskelly33@gmail.com>
2022-02-27 14:13:45 +0000

    Remove unnecessary required attrs

--- profiles/forms.py
+++ profiles/forms.py
@@ -27,9 +27,7 @@ class UserProfileForm(forms.ModelForm):
         self.fields['default_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
+                placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
