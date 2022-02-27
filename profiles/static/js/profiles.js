ccab13779aeeca803dfcf119bffe71f96b9aff54
jameskelly33 <jameskelly33@gmail.com>
2022-02-27 15:24:28 +0000

    Add extra JS to change placeholder color of Level select input

--- profiles/static/js/profiles.js
+++ profiles/static/js/profiles.js
@@ -1,4 +1,4 @@
-// Code taken from Code Institute's Project Boutique Ado https://github.com/Code-Institute-Solutions/boutique_ado_v1/tree/933797d5e14d6c3f072df31adf0ca6f938d02218
+// Code taken from Code Institute's Project Boutique Ado https://github.com/Code-Institute-Solutions/boutique_ado_v1/
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
     $('#id_default_country').css('color', '#aab7c4');
@@ -6,6 +6,19 @@ if(!countrySelected) {
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
+
+let levelSelected = $('#id_default_current_english_level').val();
+if(!levelSelected) {
+    $('#id_default_current_english_level').css('color', '#aab7c4');
+};
+$('#id_default_current_english_level').change(function() {
+    levelSelected = $(this).val();
+    if(!levelSelected) {
         $(this).css('color', '#aab7c4');
     } else {
         $(this).css('color', '#000');
